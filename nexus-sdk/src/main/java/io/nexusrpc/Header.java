f9573f610ccbff4e76e411ab0b6bf26a7aef37da
Quinn Klassen <klassenq@gmail.com>
2024-10-22 10:01:19 -0700

    Add Operation-Timeout header (#16)

--- nexus-sdk/src/main/java/io/nexusrpc/Header.java
+++ nexus-sdk/src/main/java/io/nexusrpc/Header.java
@@ -5,5 +5,11 @@ public class Header {
   /** Header for the total time to complete a Nexus request. */
   public static final String REQUEST_TIMEOUT = "Request-Timeout";
 
+  /**
+   * Total time to complete a Nexus operation. Unlike {@link Header#REQUEST_TIMEOUT}, this applies
+   * to the whole operation, not just a single HTTP request.
+   */
+  public static final String OPERATION_TIMEOUT = "Operation-Timeout";
+
   private Header() {}
 }
